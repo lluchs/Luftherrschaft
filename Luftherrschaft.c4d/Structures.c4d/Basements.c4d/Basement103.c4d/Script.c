64c603bc830fa1ab2349eca9867b0c6bb00dff35
Nachtfalter <Nachtfalter@417ea86b-d6b9-46eb-afa4-e379d024af20>
2008-02-11 20:20:03 +0000

    git-svn-id: http://opensvn.csie.org/Luftherrschaft@22 417ea86b-d6b9-46eb-afa4-e379d024af20

---
+++ Luftherrschaft.c4d/Structures.c4d/Basements.c4d/Basement103.c4d/Script.c
@@ -0,0 +1,10 @@
+/*-- Fundament --*/
+
+#strict
+
+/* Alle Fundamente leiten ihre Grundfunktionalität von BAS7 ab */
+
+#include BAS7
+
+private func BasementID() { return(L103); }
+private func BasementWidth() { return(103); }
