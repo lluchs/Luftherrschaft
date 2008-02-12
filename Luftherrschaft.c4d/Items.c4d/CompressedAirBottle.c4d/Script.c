ca2bf9a82c77bd55b1daa1c61d0a2f7739d027ee
Luchs <Luchs@417ea86b-d6b9-46eb-afa4-e379d024af20>
2008-02-12 17:28:35 +0000

    + die Heimatbasis hat jetzt Forschfunktion + der Druckluftbehälter kann Druckluft verlieren und hat tollen Sound
    git-svn-id: http://opensvn.csie.org/Luftherrschaft@28 417ea86b-d6b9-46eb-afa4-e379d024af20

--- Luftherrschaft.c4d/Items.c4d/CompressedAirBottle.c4d/Script.c
+++ Luftherrschaft.c4d/Items.c4d/CompressedAirBottle.c4d/Script.c
@@ -1,8 +1,38 @@
-/*-- Neues Objekt --*/
+/*-- Druckluftbehälter --*/
 
 #strict
 
-func Initialize() {
-   
-  return(1);
+local iDruck, iMaxDruck;
+
+protected func Initialize() {
+  iDruck = 500;
+  iMaxDruck = 500;
+  return 1;
+}
+
+public func IsFull() {
+	if(iDruck)
+		return 1;
+	return;
+}
+
+public func RemovePression(iPression) {
+	// Sound
+	Sound("pfft");
+	// wird versucht, mehr Druck abzuziehen, als beinhaltet wird?
+	if(iDruck < iPression) {
+		var Druck = iDruck; // Druck speichern
+		// leer :[
+		iDruck = 0;
+		// "abgezogener" Druck zurückgeben
+		return Druck;
+	}
+	// Druck abziehen
+	iDruck -= iPression;
+	// abgezogener Druck zurückgeben
+	return iPression;
+}
+
+protected func Activate(pClonk) {
+	return PlayerMessage(GetOwner(pClonk), "$TxtRestPression$", this, iDruck);
 }
