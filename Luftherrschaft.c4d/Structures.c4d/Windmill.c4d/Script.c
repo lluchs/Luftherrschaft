5fd0dec1a4af4cae5acefc75d2799e2e61f84169
Luchs <Luchs@417ea86b-d6b9-46eb-afa4-e379d024af20>
2008-02-12 17:29:18 +0000

    ! das Windrad *sollte* gehen (ungetestet)
    git-svn-id: http://opensvn.csie.org/Luftherrschaft@29 417ea86b-d6b9-46eb-afa4-e379d024af20

--- Luftherrschaft.c4d/Structures.c4d/Windmill.c4d/Script.c
+++ Luftherrschaft.c4d/Structures.c4d/Windmill.c4d/Script.c
@@ -17,3 +17,13 @@ private func CreateWindwing()
 {
   ObjectSetAction(CreateObject(WWNG,0,0,GetOwner()),"Turn",this());
 }
+
+/* TimerCall */
+
+private func CheckWindwing()
+{
+  // Komplett, aber kein Windrad da: reduzieren für Neuaufbau/Reparatur
+  if(GetCon() == 100)
+    if(!FindObject(WWNG,0,0,0,0,0,"Turn",this()) )
+      DoCon(-1);
+}
