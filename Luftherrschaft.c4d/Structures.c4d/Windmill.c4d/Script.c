ea2af6d0eb753b8b57a1216852f7dd805c989299
Nachtfalter <Nachtfalter@417ea86b-d6b9-46eb-afa4-e379d024af20>
2008-02-12 19:25:54 +0000

    git-svn-id: http://opensvn.csie.org/Luftherrschaft@31 417ea86b-d6b9-46eb-afa4-e379d024af20

--- Luftherrschaft.c4d/Structures.c4d/Windmill.c4d/Script.c
+++ Luftherrschaft.c4d/Structures.c4d/Windmill.c4d/Script.c
@@ -4,26 +4,7 @@
 
 #include BAS1
 
-/* Initialisierung */
-
-protected func Initialize()
+private func Initialize()
 {
-  CreateWindwing();
-}
-
-/* Windrad erzeugen */
-
-private func CreateWindwing()
-{
-  ObjectSetAction(CreateObject(WWNG,0,0,GetOwner()),"Turn",this());
-}
-
-/* TimerCall */
-
-private func CheckWindwing()
-{
-  // Komplett, aber kein Windrad da: reduzieren für Neuaufbau/Reparatur
-  if(GetCon() == 100)
-    if(!FindObject(WWNG,0,0,0,0,0,"Turn",this()) )
-      DoCon(-1);
+  ObjectSetAction(CreateObject(LWNG,0,0,GetOwner()),"Turn",this());
 }
--- Luftherrschaft.c4d/Structures.c4d/Windmill.c4d/Wing.c4d/Script.c
+++ Luftherrschaft.c4d/Structures.c4d/Windmill.c4d/Wing.c4d/Script.c
@@ -1,14 +1,4 @@
 #strict
 
-//protected func Initialize(){SetAction("Turn");return(1);}
+protected func Initialize(){SetAction("Turn");return(1);}
 
-func Completion()
-{
-  SetR(Random(360));
-}
-
-func Wind2Turn()
-{
-  DoEnergy(Abs(GetRDir() * 5), GetActionTarget() );
-  SetRDir(GetWind() / 4);
-}
