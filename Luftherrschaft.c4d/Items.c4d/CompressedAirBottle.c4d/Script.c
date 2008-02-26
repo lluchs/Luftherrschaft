81faf6a21f627961b2bffcac250c8cb51cd0f7ff
Luchs <Luchs@417ea86b-d6b9-46eb-afa4-e379d024af20>
2008-02-26 22:24:11 +0000

    + Füllstandanzeige
    git-svn-id: http://opensvn.csie.org/Luftherrschaft@54 417ea86b-d6b9-46eb-afa4-e379d024af20

--- Luftherrschaft.c4d/Items.c4d/CompressedAirBottle.c4d/Script.c
+++ Luftherrschaft.c4d/Items.c4d/CompressedAirBottle.c4d/Script.c
@@ -2,11 +2,12 @@
 
 #strict
 
-static const LCAB_MaxFill = 500;
+static const LCAB_MaxFill = 400;
 
 local iFillLevel;
 
 protected func Initialize() {
+	UpdatePicture();
   return 1;
 }
 
@@ -19,6 +20,10 @@ public func IsCompressedAirBottle() {
 	return 1;
 }
 
+
+// Aktuelle Füllung
+public func GetAmount() { return(iFillLevel); }
+
 // Maximale Füllung
 public func MaxFill() { return LCAB_MaxFill; }
 
@@ -30,10 +35,42 @@ public func DoFill(int iChange, bool fNoSound)
   var iNewFill = BoundBy(iFillLevel + iChange, 0, MaxFill());
   if (iNewFill == iFillLevel) return;
   iNewFill -= iFillLevel; iFillLevel += iNewFill;
+  UpdatePicture(); // Zahl aktualisieren
   // Tatsächliche Änderung des Füllstandes zurückgeben
   return iNewFill;
 }
 
 protected func Activate(pClonk) {
-	return PlayerMessage(GetOwner(pClonk), "$TxtRestPression$", this, iDruck);
+	return PlayerMessage(GetOwner(pClonk), "$TxtRestPression$", this, GetAmount());
+}
+
+/* Grafik anpassen */
+public func UpdatePicture()
+{
+	var iTemp;
+	if(GetAmount()>99)
+  {
+  	iTemp = GetAmount() / 100;
+  	Log("1.: %d", iTemp);
+    SetGraphics(0,0,GetNumberID(iTemp),1,GFXOV_MODE_Picture);    
+    SetObjDrawTransform(400,0,-14000,0,400,+10000, this, 1);
+  }
+  if(GetAmount()>9)
+  {
+  	iTemp = GetAmount() / 10 - GetAmount() / 100 * 10;
+  	Log("2.: %d", iTemp);
+    SetGraphics(0,0,GetNumberID(iTemp),2,GFXOV_MODE_Picture);    
+    SetObjDrawTransform(400,0,-7000,0,400,+10000, this, 2);
+  }
+  else SetGraphics(0,0,0,1,0);  
+
+  SetGraphics(0,0,GetNumberID(GetAmount() % 10),3,GFXOV_MODE_Picture);   
+  SetObjDrawTransform(400,0,0,0,400,+10000, this, 3);
+  return 1;
+}
+
+/* ID des passenden Zahlobjektes ausgeben */
+private func GetNumberID(i)
+{
+  return(C4Id(Format("SNB%d", i)));
 }
