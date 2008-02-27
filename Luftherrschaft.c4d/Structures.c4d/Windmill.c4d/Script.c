1cf6dcdfeca03c36dc80f85841ba867a9da9063b
Luchs <Luchs@417ea86b-d6b9-46eb-afa4-e379d024af20>
2008-02-27 07:49:43 +0000

    ! keine Warnings mehr beim Spielstart ! die Solidmask des Regenfängers geht jetzt (ich mach mich mal demnächst an das Script)
    git-svn-id: http://opensvn.csie.org/Luftherrschaft@55 417ea86b-d6b9-46eb-afa4-e379d024af20

--- Luftherrschaft.c4d/Structures.c4d/Windmill.c4d/Script.c
+++ Luftherrschaft.c4d/Structures.c4d/Windmill.c4d/Script.c
@@ -7,5 +7,16 @@
 private func Initialize()
 {
   ObjectSetAction(CreateObject(LWNG,0,0,GetOwner()),"Turn",this);
-  Log("Init Windrad");
+  //Log("Init Windrad");
+  return 1;
+}
+
+/* TimerCall */
+
+private func CheckWindwing()
+{
+  // Komplett, aber kein Windrad da: reduzieren für Neuaufbau/Reparatur
+  if(GetCon() == 100)
+    if(!FindObject(LWNG,0,0,0,0,0,"Turn",this()) )
+      DoCon(-1);
 }
--- Luftherrschaft.c4d/Structures.c4d/Windmill.c4d/Wing.c4d/Script.c
+++ Luftherrschaft.c4d/Structures.c4d/Windmill.c4d/Wing.c4d/Script.c
@@ -1,5 +1,14 @@
 #strict
 
-protected func Initialize(){SetAction("Turn");
-Log("Init Wing");return (1);}
+protected func Initialize(){
+	SetAction("Turn");
+	//Log("Init Wing");
+	return 1;
+}
+
+protected func Wind2Turn()
+{
+  DoEnergy(Abs(GetWind() / 4 * 5), GetActionTarget() );
+  //SetRDir(GetWind() / 4);
+}
 
