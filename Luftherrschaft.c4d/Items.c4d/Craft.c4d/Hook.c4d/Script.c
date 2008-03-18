0ad14728c3e0e76f2563d2da9346e31a96c2be1e
Luchs <Luchs@417ea86b-d6b9-46eb-afa4-e379d024af20>
2008-03-18 22:09:56 +0000

    * neuer Syntax, ungetestet
    git-svn-id: http://opensvn.csie.org/Luftherrschaft@101 417ea86b-d6b9-46eb-afa4-e379d024af20

--- Luftherrschaft.c4d/Items.c4d/Craft.c4d/Hook.c4d/Script.c
+++ Luftherrschaft.c4d/Items.c4d/Craft.c4d/Hook.c4d/Script.c
@@ -1,45 +1,59 @@
 /*-- Der Enterhaken --*/
+
 #strict
-local Rope, Seilende;       
-Departure:               
+
+local Rope, Seilende;
+    
+protected func Departure(object pContainer) {               
   SetAction ("Idle");
-  if (!GetXDir ()) return ();
-  if (Rope) return ();
-  Seilende = CreateObject (1A1E, 0, 0, GetOwner());
-  Rope = CreateObject (171E, 0, 0, GetOwner());
-  Rope -> Activate (Seilende, this ());
+  if(!GetXDir())
+  	return;
+  if(Rope)
+  	return;
+  Seilende = CreateObject(1A1E, 0, 0, GetOwner());
+  Rope = CreateObject(171E, 0, 0, GetOwner());
+  Rope -> Activate(Seilende, this);
   //ObjectSetAction (Rope,"Connect",Seilende,this());
-  ObjectCall( Seilende, "SetRope", Rope, this());
-  Enter(Par(), Seilende);
-  SetXDir( GetXDir( )* 2);
-  SetYDir( GetYDir( )* 3);
-  Sound( "Catapult");
-return( );
+  //ObjectCall(Seilende, "SetRope", Rope, this());
+  Seilende -> SetRope(Rope, this);
+  Enter(pContainer, Seilende);
+  SetXDir(GetXDir()* 2);
+  SetYDir(GetYDir()* 3);
+  Sound("Catapult");
+	return;
+}
             
-Entrance:               
-  SetAction( "Idle");
-return( );
+protected func Entrance() {               
+  SetAction("Idle");
+	return;
+}
 
-RopeBreak:                                           
-  Rope=0;                                          
-  if(Seilende) RemoveObject(Seilende);
-  SetAction( "Idle");                               
-return( );
+public func RopeBreak() {                                           
+  Rope = 0;                                          
+  if(Seilende)
+  	RemoveObject(Seilende);
+  SetAction("Idle");                               
+	return;
+}
 
-Hit:
-  Sound( "Woodhit*");
-  if(!Rope) return();
+protected func Hit() {
+  Sound("Woodhit*");
+  if(!Rope)
+  	return;
   SetXDir();
   SetYDir();
-  ObjectSetAction(Rope,"ConnectSingle",Seilende, this());
-  ObjectCall( Rope, "SetLength", ObjectCall( Rope, "CalcLength"));
-return();
+  Rope -> SetAction("ConnectSingle", Seilende, this);
+  Rope -> SetLength(Rope -> CalcLength());
+  //ObjectCall( Rope, "SetLength", ObjectCall( Rope, "CalcLength"));
+	return 1;
+}
 
-Contacting:
-  SetAction( Par());
-return( );
+private func Contacting(string szAction) {
+  SetAction(szAction);
+	return 1;
+}
 
-ContactLeft: return(Contacting("Hookleft"));
-ContactRight: return(Contacting("Hookright"));
-ContactBottom: return(Contacting("Hookdown"));
-ContactTop: return(Contacting("Hookup"));
+protected func ContactLeft()		{ return Contacting("Hookleft");	}
+protected func ContactRight()		{ return Contacting("Hookright");	}
+protected func ContactBottom()	{ return Contacting("Hookdown");	}
+protected func ContactTop()			{ return Contacting("Hookup");		}
