2c54c868c3192cbcd754e08d87ab2a869728f4bf
Nachtfalter <Nachtfalter@417ea86b-d6b9-46eb-afa4-e379d024af20>
2008-03-25 16:28:38 +0000

    * Altimeter fix | + Kristall Menüsymbol
    git-svn-id: http://opensvn.csie.org/Luftherrschaft@114 417ea86b-d6b9-46eb-afa4-e379d024af20

--- Luftherrschaft.c4d/Rules.c4d/Altimeter.c4d/Script.c
+++ Luftherrschaft.c4d/Rules.c4d/Altimeter.c4d/Script.c
@@ -16,7 +16,7 @@ protected func Activate(iByPlayer)
 
 global func CreateAltimeter(int iPlayer)
 {
- var altimeter=CreateObject(AM4Z,50,GetDefCoreVal("Height","DefCore",AM4Z)+100,iPlayer);
+ var altimeter=CreateObject(AM4Z,AbsX(50),AbsY(GetDefCoreVal("Height","DefCore",AM4Z)+100),iPlayer);
  altimeter->SetVisibility(VIS_Owner);
  altimeter->DoInit();
 }
@@ -28,3 +28,23 @@ global func RemoveAltimeter(int iPlayer)
  alti->RemoveObject();
  return 1;
 }
+
+protected func Initialize()
+{
+ for(var cnt=0;cnt<GetPlayerCount();cnt++)
+ {
+  var iPlr=GetPlayerByIndex(cnt);
+  if(!FindObject2(Find_ID(AM4Z),Find_Owner(iPlr))) CreateAltimeter(iPlr);
+ }
+} 
+
+protected func Destruction()
+{
+ if(ObjectCount2(Find_ID(AR4Z)) > 1)return;
+ for(var cnt=0;cnt<GetPlayerCount();cnt++)
+ {
+  var iPlr=GetPlayerByIndex(cnt);
+  RemoveAltimeter(iPlr);
+ }
+}
+
