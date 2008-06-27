d635f562c388e9c0735605fd75158507c47b295c
Nachtfalter <Nachtfalter@417ea86b-d6b9-46eb-afa4-e379d024af20>
2008-06-27 00:49:29 +0000

    + Arrays | +NoSpecial2 | + L450 Pt2
    git-svn-id: http://opensvn.csie.org/Luftherrschaft@187 417ea86b-d6b9-46eb-afa4-e379d024af20

---
+++ Luftherrschaft.c4d/System.c4g/Arrays.c
@@ -0,0 +1,109 @@
+/*-- Arrays --*/
+ 
+#strict
+ 
+//Löscht ein Item aus einem Array
+global func DeleteArrayItem(iNumber, &aArray)
+{
+ var temp=[];
+ for(var cnt;cnt<GetLength(aArray);cnt++)
+ {
+  if(cnt==iNumber)continue;
+  var dif=0;
+  if(cnt>iNumber)dif=-1;
+  temp[cnt+dif]=aArray[cnt];
+ } 
+ 
+ aArray=temp;
+ return(aArray);
+}
+ 
+//Löscht ein Item aus einem Array, kann möglicherweise umsortieren
+global func DeleteArrayItem2(iNumber,&aArray)
+{
+ //Ein ganz leeres Array?
+ if(GetLength(aArray)==1)return((aArray=CreateArray()));
+ //Wenn das letzte Element ist diese Funktion auch nciht toller.
+ if(GetLength(aArray)-1==iNumber)
+ return(DeleteArrayItem(iNumber, aArray));
+ 
+ //Los!
+ var last=aArray[GetLength(aArray)-1];
+ aArray[GetLength(aArray)-1]=0;
+ SetLength(aArray,GetLength(aArray)-1);
+ aArray[iNumber]=last;
+ return(aArray);
+}
+ 
+//Sucht ein Item im array
+global func GetArrayItemPosition(value,aArray)
+{
+ var number=-1;
+ for(var cnt=0;cnt<GetLength(aArray);cnt++)
+ {
+  if(aArray[cnt] == value)
+  {
+   number=cnt;
+   break;
+  }
+  else continue;
+ }
+ 
+ return(number);
+}
+ 
+//Fügt ein Item am Ende eines Array ein
+global func PushBack(value,&aArray)
+{
+ return(aArray[GetLength(aArray)]=value);
+}
+ 
+//Fügt ein Item am Anfang ein
+global func PushFront(value,&aArray)
+{
+ var aNew=[];
+ aNew[GetLength(aNew)]=value;
+ for(var cnt=0;cnt<GetLength(aArray);cnt++)
+ aNew[GetLength(aNew)]=aArray[cnt];
+ 
+ aArray=aNew;
+ return(1);
+}
+ 
+//Führt mit jedem Item im Array einen beliebigen Vergleich vor, der als String vorliegen sollte und gibt ein Array mit Ergebnissen zurück
+// zB ForEach("<0",myArray);
+//    ForEach("->~IsClonk()",myArray);
+/*global func ForEach(sFunction,aArray)
+{
+ var aResults=[];
+ 
+ Log("%d",GetLength(aArray));
+ 
+ for(var cnt=0;cnt<GetLength(aArray);cnt++)
+ {
+  //var result=
+ // eval(Format("Var(0)=(aArray[%d]%s)",cnt,sFunction));
+ Var(0)=eval(Format("(%d%s)",aArray[cnt],sFunction));
+  PushBack(Var(0),aResults);
+ }
+ 
+ return(aResults);
+}
+ 
+global func Test()
+{
+ var arr=[];
+ arr[GetLength(arr)]=1;
+ arr[GetLength(arr)]=20;
+ arr[GetLength(arr)]=34;
+ arr[GetLength(arr)]=21;
+ arr[GetLength(arr)]=564;
+ arr[GetLength(arr)]=13;
+ arr[GetLength(arr)]=4;
+ arr[GetLength(arr)]=76;
+ arr[GetLength(arr)]=124;
+ var res=ForEach("<150",arr);
+ 
+ for(var integer in res)
+ Log("%d",integer);
+}*/
---
+++ Luftherrschaft.c4d/System.c4g/NoSpecial2.c
@@ -0,0 +1,23 @@
+/*-- Damit man nicht das System beim Fliegenden Händler austricksen kann --*/
+ 
+#strict
+#appendto CLNK
+ 
+protected func ControlSpecial2()
+{
+  [$CtrlMenuDesc$|Image=CXTX]
+  // In einem Gebäude oder Fahrzeug: das Kontextmenü des Gebäudes öffnen
+  if (Contained())
+    if ((Contained()->GetCategory() & C4D_Structure) || (Contained()->GetCategory() & C4D_Vehicle))
+     if (Contained()->GetID() != LSMN)
+      return(SetCommand(this(),"Context",0,0,0,Contained()), ExecuteCommand());
+  // Fasst ein Objekt an: Kontextmenü des angefassten Objekts öffnen
+  if (GetAction() eq "Push")
+   if (GetActionTarget()->GetID() != LSMN)
+    return(SetCommand(this(),"Context",0,0,0,GetActionTarget()), ExecuteCommand());
+  // Trägt ein Objekt: Kontextmenü des ersten getragenen Objekts öffnen
+  if (Contents(0))
+    return(SetCommand(this(),"Context",0,0,0,Contents(0)), ExecuteCommand());
+  // Ansonsten das Kontextmenü des Clonks öffnen
+  return(SetCommand(this(),"Context",0,0,0,this()), ExecuteCommand());
+}
