303c3c56fd69dc9e696c50a2391cd4e1e57f9704
Nachtfalter <Nachtfalter@417ea86b-d6b9-46eb-afa4-e379d024af20>
2008-06-27 00:35:46 +0000

    * Testszen Materialfix | * Schiffschmiede Vertices Fix
    git-svn-id: http://opensvn.csie.org/Luftherrschaft@185 417ea86b-d6b9-46eb-afa4-e379d024af20

---
+++ Luftherrschaft.c4d/Structures.c4d/Basements.c4d/Basement053.c4d/Script.c
@@ -0,0 +1,10 @@
+/*-- Fundament --*/
+
+#strict
+
+/* Alle Fundamente leiten ihre Grundfunktionalität von BAS7 ab */
+
+#include BAS7
+
+private func BasementID() { return(L053); }
+private func BasementWidth() { return(53); }
