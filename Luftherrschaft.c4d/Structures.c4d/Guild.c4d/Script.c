2f694acf1cb5b8f69533d426d7347dacd9864e97
Luchs <Luchs@417ea86b-d6b9-46eb-afa4-e379d024af20>
2008-08-21 19:47:36 +0000

    + Die Gilde nimmt nun Objekte entgegen, die aus Komponenten bestehen, die vom Materialsystem verwaltet werden (z.B. Holz, Bäume) und zerlegt diese ins Materialsystem. Wenn ein Container (z.B. Lore) hineingebracht wird, werden die passenden Items in der Lore zerlegt und diese ausgeworfen. Wenn der Container selbst zerlegt werden soll, muss er leer sein. 2do: aufgrund des Systemes kann bisher nur zu Holz (und andere Objekte, die als Komponenten bestehen und das Materialsystem diese verwaltet) zerlegt werden, daher müssen Spezialfälle eingebaut werden (was ich mache, wenn ich vom Urlaub zurück bin)
    git-svn-id: http://opensvn.csie.org/Luftherrschaft@254 417ea86b-d6b9-46eb-afa4-e379d024af20

--- Luftherrschaft.c4d/Structures.c4d/Guild.c4d/Script.c
+++ Luftherrschaft.c4d/Structures.c4d/Guild.c4d/Script.c
@@ -1,10 +1,108 @@
 /*-- Gildet --*/
 
-#strict
+#strict 2
 #include L103
 #include DOOR
 
-func Initialize() {
-   
-  return(1);
+local hComponents, hContainer;
+
+protected func Initialize() {
+	hComponents = CreateHash();
+	hContainer = CreateHash();
+	SetEntrance();
+}
+
+private func OnlyMatSysComponents(object pObj, array &aIDs) {
+	var fFlag, idComponent, ID;
+	for(var i = 0; idComponent = GetComponent(0, i, pObj); i++) {
+		for(ID in GetMatSysIDs())
+			if(ID == idComponent) {
+				fFlag = true;
+				break;
+			}
+		if(!fFlag)
+			return;
+		if(GetType(aIDs))
+			aIDs[i] = idComponent;
+	}
+	return 1;
+}
+
+private func ContainedMatSysComponents(object pContainer, array &hash, array &aContainer) {
+	var fFlag;
+	for(var pObj in FindObjects(Find_Container(pContainer))) {
+		HashPut(hash, pObj, []);
+		if(OnlyMatSysComponents(pObj, HashGet(hash, pObj))) {
+			fFlag = true;
+			PushBack(pObj, aContainer);
+			continue; // alle Components müssen markiert werden
+		}
+		HashErase(hash, pObj);
+	}
+	return fFlag;
+}
+
+private func OpenDoor(object pObj, bool fExit) {
+	SetEntrance(1);
+	if(fExit)
+		ExitCheck(pObj);
+	return 1;
+}
+
+protected func ExitCheck(object pObj) {
+	if(!Contained(pObj))
+		return SetEntrance();
+	ScheduleCall(this, "ExitCheck", 5);
+}
+
+protected func ActivateEntrance(object pObj) {
+	if(GetCommand(pObj) == "Exit")
+		return OpenDoor(pObj, true);
+
+	if(GetOCF(pObj) & OCF_CrewMember) {
+		var aDummy = CreateArray();
+		ContainedMatSysComponents(pObj, hComponents, aDummy);
+		return OpenDoor();
+	}
+
+	HashPut(hContainer, pObj, []);
+	if(ContainedMatSysComponents(pObj, hComponents, HashGet(hContainer, pObj)))
+		return OpenDoor();
+	HashErase(hContainer, pObj);
+
+	HashPut(hComponents, pObj, []);
+	if(OnlyMatSysComponents(pObj, HashGet(hComponents, pObj))) {
+		// Hier evtl. Größenüberprüfung (und Anpassung)
+		return OpenDoor();
+	}
+	HashErase(hComponents, pObj);
+
+	SetCommand(pObj, "None");
+	return;
+}
+
+protected func Collection2(object pObj) {
+	SetEntrance();
+	if(GetOCF(pObj) & OCF_CrewMember) {
+		if(GetOwner() == NO_OWNER)
+			SetOwner(GetOwner(pObj));
+		/*for(var pObject in FindObjects(Find_Container(pObj)))
+			if(HashGet(hComponents, pObject))
+				Collection2(pObject);*/
+		return;
+	}
+	var aContents = HashGet(hContainer, pObj);
+	if(aContents)
+		for(var pObject in aContents)
+			Collection2(pObject);
+	var aComponents = HashGet(hComponents, pObj);
+	if(!aComponents && GetRealContainer(pObj) == this)
+		return SetCommand(pObj, "Exit");
+	if(!aComponents && GetRealContainer(pObj) != this)
+		return;
+	for(idComponent in aComponents)
+		MatSysDoFill(GetComponent(idComponent, 0, pObj), GetOwner(), idComponent);
+	HashErase(hComponents, pObj);
+	RemoveObject(pObj);
+	return 1;
 }
