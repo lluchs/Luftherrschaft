065583fd2b30fce6fdb07e6fb3321426ae9515de
Luchs <Luchs@417ea86b-d6b9-46eb-afa4-e379d024af20>
2008-10-01 19:04:23 +0000

    * Script -> #strict 2
    git-svn-id: http://opensvn.csie.org/Luftherrschaft@277 417ea86b-d6b9-46eb-afa4-e379d024af20

--- Luftherrschaft.c4d/Helpers.c4d/appendtoDIV.c4d/Script.c
+++ Luftherrschaft.c4d/Helpers.c4d/appendtoDIV.c4d/Script.c
@@ -1,9 +1,11 @@
-#strict
+#strict 2
 #appendto BLMP
 #appendto BALN
 #appendto TREE
 
-public func GetVertexToConnect () {
-  if (this () ->~ IsTree ()) return (2);
-  if (GetID () == BLMP || GetID () == BALN) return (-1);
+public func GetVertexToConnect() {
+  if(this -> ~IsTree())
+  	return 2;
+  if(GetID() == BLMP || GetID() == BALN)
+  	return -1;
 }
